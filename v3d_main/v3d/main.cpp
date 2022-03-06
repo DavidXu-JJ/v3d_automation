bdb5e8986cb384d6130026ec0d1c149460d86a8e
徐京伟 <2374539357@qq.com>
2022-03-06 11:57:20 +0800

    auto select axis

--- v3d_main/v3d/main.cpp
+++ v3d_main/v3d/main.cpp
@@ -1651,33 +1651,72 @@ void App2_non_recursive_DFS(const int & blocksize,const QString & File_Name){
     writeSWC_file(Work_Dir+QString("/")+File_Name,output);
 }
 
-struct SliceXY{
-    int x,y;
+struct Slice{
+    int dim1,dim2;
     QVector<QVector<int> > mp;
-    SliceXY(int _x,int _y):x(_x),y(_y){
-        mp = QVector<QVector<int> > (x,QVector<int> (y));
+    Slice(int _dim1,int _dim2):dim1(_dim1),dim2(_dim1){
+        mp = QVector<QVector<int> > (dim1,QVector<int> (dim2));
     }
 };
 
-SliceXY Get_SliceXY(unsigned char * indata1d,const int & z,const int & blocksize){
-    SliceXY ret(blocksize,blocksize);
+//Slice Get_Slice(unsigned char * indata1d,const int & dim3,const int & blocksize,const int & mode){
+//    Slice ret(blocksize,blocksize);
+//    if(mode==0){
+//    for(int i=0;i<blocksize;++i)
+//        for(int j=0;j<blocksize;++j){
+//            ret.mp[i][j]=(int)(indata1d[Get_Marker_Idx(i,j,dim3,blocksize)]);
+//        }
+//    }else{
+//        if(mode==1){
+//            for(int i=0;i<blocksize;++i)
+//                for(int j=0;j<blocksize;++j){
+//                    ret.mp[i][j]=(int)(indata1d[Get_Marker_Idx(dim3,i,j,blocksize)]);
+//                }
+//        }else{
+//            if(mode==2){
+//                for(int i=0;i<blocksize;++i)
+//                    for(int j=0;j<blocksize;++j){
+//                        ret.mp[i][j]=(int)(indata1d[Get_Marker_Idx(i,dim3,j,blocksize)]);
+//                    }
+//            }
+//        }
+//    }
+//    return ret;
+//}
+Slice Get_SliceXY(unsigned char * indata1d,const int & dim3,const int & blocksize){
+    Slice ret(blocksize,blocksize);
     for(int i=0;i<blocksize;++i)
         for(int j=0;j<blocksize;++j){
-            ret.mp[i][j]=(int)(indata1d[Get_Marker_Idx(i,j,z,blocksize)]);
+            ret.mp[i][j]=(int)(indata1d[Get_Marker_Idx(i,j,dim3,blocksize)]);
+        }
+    return ret;
+}
+Slice Get_SliceYZ(unsigned char * indata1d,const int & dim3,const int & blocksize){
+    Slice ret(blocksize,blocksize);
+    for(int i=0;i<blocksize;++i)
+        for(int j=0;j<blocksize;++j){
+            ret.mp[i][j]=(int)(indata1d[Get_Marker_Idx(dim3,i,j,blocksize)]);
+        }
+    return ret;
+}Slice Get_SliceXZ(unsigned char * indata1d,const int & dim3,const int & blocksize){
+    Slice ret(blocksize,blocksize);
+    for(int i=0;i<blocksize;++i)
+        for(int j=0;j<blocksize;++j){
+            ret.mp[i][j]=(int)(indata1d[Get_Marker_Idx(i,dim3,j,blocksize)]);
         }
     return ret;
 }
 
-QVector<XYZ> Summary_SliceXY(const SliceXY & slice,const int & z){
+QVector<XYZ> Summary_SliceXY(const Slice & slice,const int & dim3){
     const int threshold = 15;
-    const int & x = slice.x;
-    const int & y = slice.y;
+    const int & dim1 = slice.dim1;
+    const int & dim2 = slice.dim2;
     QVector<XYZ> points;
     QVector<int> intensity;
-    for(int i=0;i<x;++i){
-        for(int j=0;j<y;++j){
+    for(int i=0;i<dim1;++i){
+        for(int j=0;j<dim2;++j){
             if(slice.mp[i][j]>=threshold){
-                points.push_back(XYZ(i,j,z));
+                points.push_back(XYZ(i,j,dim3));
                 intensity.push_back((int)(slice.mp[i][j]));
             }
         }
@@ -1695,7 +1734,7 @@ QVector<XYZ> Summary_SliceXY(const SliceXY & slice,const int & z){
                 XYZ now = q.front();
                 q.pop_front();
                 for(int i=0;i<points.size();++i){
-                   if(!vis[i] && distance_XYZ(now,points[i])<=8){
+                   if(!vis[i] && distance_XYZ(now,points[i])<=5){
                       Group.push_back(points[i]);
                       Vec_Intensity.push_back(intensity[i]);
                       vis[i]=true;
@@ -1728,13 +1767,135 @@ QVector<XYZ> Summary_SliceXY(const SliceXY & slice,const int & z){
     return ret;
 }
 
-V_NeuronSWC_list Summary_Points(unsigned char * indata1d,const int & blocksize){
+QVector<XYZ> Summary_SliceYZ(const Slice & slice,const int & dim3){
+    const int threshold = 15;
+    const int & dim1 = slice.dim1;
+    const int & dim2 = slice.dim2;
+    QVector<XYZ> points;
+    QVector<int> intensity;
+    for(int i=0;i<dim1;++i){
+        for(int j=0;j<dim2;++j){
+            if(slice.mp[i][j]>=threshold){
+                points.push_back(XYZ(dim3,i,j));
+                intensity.push_back((int)(slice.mp[i][j]));
+            }
+        }
+    }
+    QMap<int,bool> vis;
+    QVector<QVector<XYZ> > Groups;
+    QVector<QVector<int> > intensities;
+    for(int i=0;i<points.size();++i){
+        if(!vis[i]){
+            QVector<XYZ> Group;
+            QVector<int> Vec_Intensity;
+            QQueue<XYZ> q;
+            q.push_back(points[i]);
+            while(!q.empty()){
+                XYZ now = q.front();
+                q.pop_front();
+                for(int i=0;i<points.size();++i){
+                   if(!vis[i] && distance_XYZ(now,points[i])<=5){
+                      Group.push_back(points[i]);
+                      Vec_Intensity.push_back(intensity[i]);
+                      vis[i]=true;
+                      q.push_back(points[i]);
+                   }
+                }
+            }
+            Groups.push_back(Group);
+            intensities.push_back(Vec_Intensity);
+        }
+    }
+    QVector<XYZ> ret;
+    for(int k=0;k<Groups.size();++k){
+        XYZ Summary_Point(0.0);
+        int M=0;
+        for(int i=0;i<Groups[k].size();++i){
+            M += intensities[k][i];
+        }
+        for(int i=0;i<Groups[k].size();++i){
+            Summary_Point = Summary_Point + Groups[k][i]*intensities[k][i] / M;
+        }
+//        for(int i=0;i<Groups[k].size();++i){
+//            Summary_Point = Summary_Point + Groups[k][i]/Groups[k].size();
+//        }
+        Summary_Point.x=(int)(Summary_Point.x+0.5);
+        Summary_Point.y=(int)(Summary_Point.y+0.5);
+        Summary_Point.z=(int)(Summary_Point.z+0.5);
+        ret.push_back(Summary_Point);
+    }
+    return ret;
+}
+
+QVector<XYZ> Summary_SliceXZ(const Slice & slice,const int & dim3){
+    const int threshold = 15;
+    const int & dim1 = slice.dim1;
+    const int & dim2 = slice.dim2;
+    QVector<XYZ> points;
+    QVector<int> intensity;
+    for(int i=0;i<dim1;++i){
+        for(int j=0;j<dim2;++j){
+            if(slice.mp[i][j]>=threshold){
+                points.push_back(XYZ(i,dim3,j));
+                intensity.push_back((int)(slice.mp[i][j]));
+            }
+        }
+    }
+    QMap<int,bool> vis;
+    QVector<QVector<XYZ> > Groups;
+    QVector<QVector<int> > intensities;
+    for(int i=0;i<points.size();++i){
+        if(!vis[i]){
+            QVector<XYZ> Group;
+            QVector<int> Vec_Intensity;
+            QQueue<XYZ> q;
+            q.push_back(points[i]);
+            while(!q.empty()){
+                XYZ now = q.front();
+                q.pop_front();
+                for(int i=0;i<points.size();++i){
+                   if(!vis[i] && distance_XYZ(now,points[i])<=5){
+                      Group.push_back(points[i]);
+                      Vec_Intensity.push_back(intensity[i]);
+                      vis[i]=true;
+                      q.push_back(points[i]);
+                   }
+                }
+            }
+            Groups.push_back(Group);
+            intensities.push_back(Vec_Intensity);
+        }
+    }
+    QVector<XYZ> ret;
+    for(int k=0;k<Groups.size();++k){
+        XYZ Summary_Point(0.0);
+        int M=0;
+        for(int i=0;i<Groups[k].size();++i){
+            M += intensities[k][i];
+        }
+        for(int i=0;i<Groups[k].size();++i){
+            Summary_Point = Summary_Point + Groups[k][i]*intensities[k][i] / M;
+        }
+//        for(int i=0;i<Groups[k].size();++i){
+//            Summary_Point = Summary_Point + Groups[k][i]/Groups[k].size();
+//        }
+        Summary_Point.x=(int)(Summary_Point.x+0.5);
+        Summary_Point.y=(int)(Summary_Point.y+0.5);
+        Summary_Point.z=(int)(Summary_Point.z+0.5);
+        ret.push_back(Summary_Point);
+    }
+    return ret;
+}
+
+V_NeuronSWC_list Summary_Points(unsigned char * indata1d,const int & blocksize,
+                                Slice (*Get_Slice)(unsigned char * indata1d,const int & dim3,const int & blocksize),
+                                QVector<XYZ> (*Summary_Slice)(const Slice & slice,const int & dim3)){
     V_NeuronSWC_list ret;
     const int threshold = 5;
-    QVector<XYZ> pre = Summary_SliceXY(Get_SliceXY(indata1d,0,blocksize),0);
+    QVector<XYZ> pre = Summary_Slice(Get_Slice(indata1d,0,blocksize),0);
 
     for(int z=3;z<blocksize;z+=3){
-        QVector<XYZ> now=Summary_SliceXY(Get_SliceXY(indata1d,z,blocksize),z);
+        QVector<XYZ> now=Summary_Slice(Get_Slice(indata1d,z,blocksize),z);
         for(int i=0;i<pre.size();++i){
             for(int j=0;j<now.size();++j){
                 XYZ vec = (now[j]-pre[i]);
@@ -1769,6 +1930,44 @@ V_NeuronSWC_list Summary_Points(unsigned char * indata1d,const int & blocksize){
             }
         }
         pre=now;
+    }
+    return ret;
+}
+
+V_NeuronSWC_list Sparse_BBox(unsigned char * indata1d,const int & blocksize){
+    const int threshold = 15;
+    QVector<XYZ> points;
+    for(int i=0;i<blocksize;++i){
+        for(int j=0;j<blocksize;++j){
+            for(int k=0;k<blocksize;++k){
+               if( (int)(indata1d[Get_Marker_Idx(i,j,k,blocksize)]) >= threshold){
+                    points.push_back(XYZ(i,j,k));
+               }
+            }
+        }
+    }
+    double meanx=0,meany=0,meanz=0;
+    double varx=0,vary=0,varz=0;
+    int sz=points.size();
+    for(const XYZ & point:points){
+        meanx+=point.x/sz;
+        meany+=point.y/sz;
+        meanz+=point.z/sz;
+    }
+    for(const XYZ & point:points){
+        varx+=(point.x-meanx)*(point.x-meanx)/sz;
+        vary+=(point.y-meany)*(point.y-meany)/sz;
+        varz+=(point.z-meanz)*(point.z-meanz)/sz;
+    }
+    V_NeuronSWC_list ret;
+    if(varx>vary&&varx>varz){
+        ret = Summary_Points(indata1d,blocksize,Get_SliceYZ,Summary_SliceYZ);
+    }else{
+        if(vary>varz){
+            ret = Summary_Points(indata1d,blocksize,Get_SliceXZ,Summary_SliceXZ);
+        } else {
+            ret = Summary_Points(indata1d,blocksize,Get_SliceXY,Summary_SliceXY);
+        }
     }
     return ret;
 }
@@ -1808,7 +2007,7 @@ int main(int argc, char **argv)
     p4dImage.loadImage(v3draw_File.toStdString().c_str(),true);
     unsigned char * indata1d = p4dImage.getRawDataAtChannel(0);
 
-    V_NeuronSWC_list out = Summary_Points(indata1d,blocksize);
+    V_NeuronSWC_list out = Sparse_BBox(indata1d,blocksize);
 
     NeuronTree outtree=V_NeuronSWC_list__2__NeuronTree(out);
     writeSWC_file("/Users/davidxu/test.eswc",outtree);
